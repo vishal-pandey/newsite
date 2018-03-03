9e3595123fd8a224424ee1a9221de265aa6dae74
vishal-pandey <pandeyvishal25@yahoo.com>
2018-03-03 20:39:02 +0530

    first draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  mobile:boolean = false;
+  constructor(breakpointObserver: BreakpointObserver){
+  	breakpointObserver.observe([
+      Breakpoints.HandsetLandscape,
+      Breakpoints.HandsetPortrait
+    ]).subscribe(result => {
+      if (result.matches) {
+        this.mobile = true;
+      }
+    });
+  }
 }
