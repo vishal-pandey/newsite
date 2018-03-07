fd4d6886c6ac6f2318039244a94a26ecb641168b
vishal-pandey <pandeyvishal25@yahoo.com>
2018-03-08 01:48:43 +0530

    update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { tween, styler } from 'popmotion';
 
 // import $ = require("jquery");
-import * as $ from "jquery"
+// import * as $ from "jquery"
 
 @Component({
   selector: 'app-home',
@@ -17,46 +17,46 @@ export class HomeComponent implements OnInit {
   constructor() {
   }
 
-  hi(){
-  	$('.hi').click(function(){
-  		$(this).animate({
-  			height: '200px',
-  			width: '200px',
-  			borderRadius: '200px',
-  			fontSize: '50px'
+  // hi(){
+  // 	$('.hi').click(function(){
+  // 		$(this).animate({
+  // 			height: '200px',
+  // 			width: '200px',
+  // 			borderRadius: '200px',
+  // 			fontSize: '50px'
 
-  		},function(){
-  			$(this).animate({
-  				borderRadius: '100px',
-  				height: '100px',
-	  			width: '100px',
-	  			fontSize: '28px'
-  			})
-  		})
+  // 		},function(){
+  // 			$(this).animate({
+  // 				borderRadius: '100px',
+  // 				height: '100px',
+	 //  			width: '100px',
+	 //  			fontSize: '28px'
+  // 			})
+  // 		})
 
-  	})
-  }
-  appear(){
-  	$('.hi').click(function(){
+  // 	})
+  // }
+  // appear(){
+  // 	$('.hi').click(function(){
   		
-  		$('.service1').animate({
-  			// display: 'initial',
-  			fontSize: '18px',
-  			height: '200px',
-  			width: '200px'
-  		},function(){
-  			$(this).animate({
-  				borderRadius: '100px',
-  				height: '100px',
-	  			width: '100px'
-  			})
-  		})
-  	})
-  }
+  // 		$('.service1').animate({
+  // 			// display: 'initial',
+  // 			fontSize: '18px',
+  // 			height: '200px',
+  // 			width: '200px'
+  // 		},function(){
+  // 			$(this).animate({
+  // 				borderRadius: '100px',
+  // 				height: '100px',
+	 //  			width: '100px'
+  // 			})
+  // 		})
+  // 	})
+  // }
 
   ngOnInit() {
-  	this.hi()
-  	this.appear()
+  	// this.hi()
+  	// this.appear()
   }
 
 }
